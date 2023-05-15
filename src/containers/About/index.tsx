d27c63f8931396f606ab65e44de7f3606d66d898
Anom <anom@anom.com>
2023-05-15 17:16:26 -0300

    Construção da sessão sobre mim

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Paragraph from '../../components/Paragraph'
+import { GithubSession } from './styles'
 
 const About = () => (
   <section>
@@ -10,6 +11,10 @@ const About = () => (
       Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum
       Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
     </Paragraph>
+    <GithubSession>
+      <img src="https://github-readme-stats.vercel.app/api?username=MisterConscio&show_icons=true&theme=onedark&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MisterConscio&layout=compact&langs_count=7&theme=onedark" />
+    </GithubSession>
   </section>
 )
 
